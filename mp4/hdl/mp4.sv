0e2f57c97b714133fe999d9bb662e50f8a5319e0
Aelita <tingkai.17@intl.zju.edu.cn>
2020-11-10 23:16:27 +0800

    Connect mp4 cp1 to test

--- mp4/hdl/mp4.sv
+++ mp4/hdl/mp4.sv
@@ -1,3 +1,24 @@
-module mp4();
+module mp4(
+    input logic clk, rst,
+
+    // Memory
+    output rv32i_word inst_mem_address,
+    output logic inst_mem_read,
+    output logic inst_mem_write,
+    output logic [3:0] inst_mem_byte_enable,
+    input rv32i_word inst_mem_rdata,
+    output rv32i_word inst_mem_wdata,
+    input logic inst_mem_resp,
+
+    output rv32i_word data_mem_address,
+    output logic data_mem_read,
+    output logic data_mem_write,
+    output logic [3:0] data_mem_byte_enable,
+    input rv32i_word data_mem_rdata,
+    output rv32i_word data_mem_wdata,
+    input logic data_mem_resp
+);
+
+cpu cpu0(.*);
 
 endmodule : mp4
--- mp4/hvl/top.sv
+++ mp4/hvl/top.sv
@@ -1,3 +1,6 @@
+import rv32i_types::*;
+import rv32i_packet::*;
+
 module mp4_tb;
 `timescale 1ns/10ps
 
@@ -21,11 +24,38 @@ bit f;
 
 /****************************** End do not touch *****************************/
 
+mp4 dut(
+    .clk                    (itf.clk), 
+    .rst                    (itf.rst),
+
+    // Memory
+    .inst_mem_address       (itf.inst_addr),
+    .inst_mem_read          (itf.inst_read),
+    .inst_mem_write         (), // hang
+    .inst_mem_byte_enable   (), // hang
+    .inst_mem_rdata         (itf.inst_rdata),
+    .inst_mem_wdata         (), // hang
+    .inst_mem_resp          (itf.inst_resp),
+
+    .data_mem_address       (itf.data_addr),
+    .data_mem_read          (itf.data_read),
+    .data_mem_write         (itf.data_write),
+    .data_mem_byte_enable   (itf.data_mbe),
+    .data_mem_rdata         (itf.data_rdata),
+    .data_mem_wdata         (itf.data_wdata),
+    .data_mem_resp          (itf.data_resp)
+);
+
 /************************ Signals necessary for monitor **********************/
 // This section not required until CP2
 
 assign rvfi.commit = 0; // Set high when a valid instruction is modifying regfile or PC
-assign rvfi.halt = 0;   // Set high when you detect an infinite loop
+
+// Set high when you detect an infinite loop
+assign rvfi.halt = dut.cpu.EX0.ex_in.inst.opcode == op_br && 
+                    (dut.cpu.EX0.ex_in.inst.rs1 == dut.cpu.EX0.ex_in.inst.rs2) && 
+                    (dut.cpu.EX0.ex_in.data.pc == dut.cpu.EX0.alu_out);   
+
 initial rvfi.order = 0;
 always @(posedge itf.clk iff rvfi.commit) rvfi.order <= rvfi.order + 1; // Modify for OoO
 
@@ -57,6 +87,23 @@ Memory:
 
 Please refer to rvfi_itf.sv for more information.
 */
+
+// assign rvfi.inst = dut.cpu.WB0.wb_in.data.instruction;
+// // assign rvfi.trap = 
+// assign rvfi.rs1_addr = dut.cpu.WB0.wb_in.inst.rs1;
+// assign rvfi.rs2_addr = dut.cpu.WB0.wb_in.inst.rs2;
+// assign rvfi.rs1_rdata = dut.cpu.WB0.wb_in.data.rs1_out;
+// assign rvfi.rs2_rdata = dut.cpu.WB0.wb_in.data.rs2_out;
+// assign rvfi.load_regfile = dut.cpu.WB0.wb_in.ctrl.load_regfile;
+// assign rvfi.rd_addr = dut.cpu.WB0.wb_in.inst.rd;
+// assign rvfi.rd_wdata = dut.cpu.WB0.regfile_in;
+// assign rvfi.pc_rdata = 
+// assign rvfi.pc_wdata = 
+// assign rvfi.mem_addr = 
+// assign rvfi.mem_rmask = 
+// assign rvfi.mem_wmask = 
+// assign rvfi.mem_rdata = 
+// assign rvfi.mem_wdata = 
 
 /**************************** End RVFIMON signals ****************************/
 
@@ -105,7 +152,13 @@ Burst Memory Ports:
 Please refer to tb_itf.sv for more information.
 */
 
-mp4 dut();
+assign itf.mem_read = itf.data_read;
+assign itf.mem_write = itf.data_write;
+assign itf.mem_addr = itf.data_addr;
+assign itf.mem_wdata = itf.data_wdata;
+assign itf.mem_resp = itf.data_resp;
+assign itf.mem_rdata = itf.data_rdata;
+
 /***************************** End Instantiation *****************************/
 
 endmodule
