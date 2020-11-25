6b9fb0d677197a6b823aa341ee41f2ab5274d3eb
Zhi <zhicen2@illinois.edu>
2020-11-25 17:19:29 +0800

    bhr use reg

--- mp4/hdl/cpu/branch_predictor/branch_predictor.sv
+++ mp4/hdl/cpu/branch_predictor/branch_predictor.sv
@@ -38,15 +38,15 @@ assign if_pred_pc = (pht_out_pred[1]) ? btb_out_pred : if_pc + 4;
 
 // for update tables
 logic actl_take;
-assign actl_take = (wb_pkt.data.pc + 4 == wb_pkt.data.next_pc) ? 0 : 1;
-logic write;
-assign write = (load_buffer && wb_pkt.valid && 
-    ((wb_pkt.inst.opcode == op_br) || (wb_pkt.inst.opcode == op_jal) || (wb_pkt.inst.opcode == op_jalr))) ? 1 : 0;
+assign actl_take = (wb_pkt.data.pc + 4 == wb_pkt.data.next_pc) ? 1'b0 : 1'b1;
+logic update;
+assign update = (load_buffer && wb_pkt.valid && 
+    ((wb_pkt.inst.opcode == op_br) || (wb_pkt.inst.opcode == op_jal) || (wb_pkt.inst.opcode == op_jalr))) ? 1'b1 : 1'b0;
 assign rindex = wb_pkt.data.pc[s_bhr-1:0] ^ bhr_out;
 
 // update logic
 always_ff @(posedge clk) begin
-    if (write) begin
+    if (update) begin
         load <= 1'b1;
         windex <= wb_pkt.data.pc[s_bhr-1:0] ^ bhr_out;
         bhr_in <= (bhr_out << 1) | {{(s_bhr-1){1'b0}}, actl_take};
@@ -70,10 +70,14 @@ always_ff @(posedge clk) begin
     end 
     else begin
         load <= 1'b0;
+        windex <= windex;
+        bhr_in <= bhr_out;
+        pht_in <= pht_out;
+        btb_in <= btb_out;
     end
 end
 
-array #(1, s_bhr)  BHR (.*, .load, .rindex(1'b0), .windex(1'b0), .datain(bhr_in), .dataout(bhr_out));
+bhr_reg   #(s_bhr) BHR (.*, .load, .in(bhr_in), .out(bhr_out));
 pht_array #(s_bhr) PHT (.*, .load, .bpindex, .rindex, .windex, .datain(pht_in), .bpdataout(pht_out_pred), .dataout(pht_out));
 btb_array #(s_bhr) BTB (.*, .load, .bpindex, .rindex, .windex, .datain(btb_in), .bpdataout(btb_out_pred), .dataout(btb_out));
 
